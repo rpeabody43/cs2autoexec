5bcdc3ecb71a9453181bb5e814ba61cbf3994b6d
rpeabody43 <rpeabody43@users.noreply.github.com>
2023-10-19 03:20:08 +0000

    deploy: e5d5343c7eb4214fd7f04ec4915fc917dff31716

--- cs2-autoexec-314d4cb185e6a156.js
+++ cs2-autoexec-314d4cb185e6a156.js
@@ -669,12 +669,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper537 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 201, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper538 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 202, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper765 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 290, __wbg_adapter_21);
+    imports.wbg.__wbindgen_closure_wrapper766 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 291, __wbg_adapter_21);
         return addHeapObject(ret);
     };
 
@@ -716,7 +716,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('cs2-autoexec-73c98cedac112df8_bg.wasm', import.meta.url);
+        input = new URL('cs2-autoexec-314d4cb185e6a156_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
