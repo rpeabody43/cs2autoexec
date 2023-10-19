01a0262b4a12c8df32342c85f173678623b54f2b
rpeabody43 <rpeabody43@users.noreply.github.com>
2023-10-19 04:16:19 +0000

    deploy: 085af8b209dc80f386076f5be87c9508f04206ee

--- cs2-autoexec-7a663e2336355354.js
+++ cs2-autoexec-7a663e2336355354.js
@@ -716,7 +716,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('cs2-autoexec-314d4cb185e6a156_bg.wasm', import.meta.url);
+        input = new URL('cs2-autoexec-7a663e2336355354_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
