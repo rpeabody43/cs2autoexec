f4309d02a42bf2484460bc293ed014b3d0a1442f
Ryan Peabody <rpeabody43@gmail.com>
2025-01-30 02:07:16 -0500

    Change from plugin to installing trunk every time

--- .netlify/plugins/install-trunk/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    onPreBuild: async () => {
-        console.log("Installing Trunk...");
-        await require("child_process").execSync("cargo install trunk", { stdio: "inherit" });
-    }
-};
